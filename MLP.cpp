ed1a1fc0890fd3f331433f8df458ad31111755f7
Ndombou jerry <ndomboukamdem@gmail.com>
2025-10-04 23:13:24 +0100

    fully connected update

--- MLP.cpp
+++ MLP.cpp
@@ -1,67 +1,47 @@
 #include <iostream>
 #include <Eigen/Dense>
-#include <fstream>
 
-int flat[9] = {0};
-Eigen::MatrixXd input(1, 9);
+using namespace Eigen;
 
-// Corrected dimensions - biases should be row vectors
-Eigen::MatrixXd layer1(9, 2);
-Eigen::RowVectorXd layer1_b(2);  // Changed to RowVectorXd
-Eigen::MatrixXd layer1_out(1, 2);  // Fixed: should be (1, 2) not (1, 9)
+int N = 3, M = 4, O = 3, P = 3;
 
-Eigen::MatrixXd layer2(2, 3);
-Eigen::RowVectorXd layer2_b(3);   // Changed to RowVectorXd
-Eigen::MatrixXd layer2_out(1, 3);
+MatrixXd inputs(O,M);
 
-Eigen::MatrixXd layer3(3, 2);
-Eigen::RowVectorXd layer3_b(2);   // Changed to RowVectorXd
-Eigen::MatrixXd layer3_out(1, 2);
+MatrixXd weights(N, M);
+RowVectorXd biases(N);
+MatrixXd layer1_outputs(O, N);
 
-Eigen::MatrixXd relu(Eigen::MatrixXd x) {
-    return x.cwiseMax(0);
-}
+MatrixXd weights2(P, N);
+RowVectorXd biases2(P);
+MatrixXd layer2_outputs(O, P);
 
-void lireFichier() {
-    std::ifstream file("conv_output.txt");
-    if(file.is_open()) {
-        for(int i = 0; i < 9; i++) {
-            file >> flat[i];
-        }
-        file.close();
-    }
-}
-
-void flat_to_input() {
-    for(int i = 0; i < 9; i++) {
-        input(0, i) = flat[i];
-    }
-}
 
 int main() {
-    lireFichier();
-    flat_to_input();
 
-    // Initialize weights and biases (important!)
-    layer1 = Eigen::MatrixXd::Random(9, 2);
-    layer1_b = Eigen::RowVectorXd::Random(2);
-    
-    layer2 = Eigen::MatrixXd::Random(2, 3);
-    layer2_b = Eigen::RowVectorXd::Random(3);
-    
-    layer3 = Eigen::MatrixXd::Random(3, 2);
-    layer3_b = Eigen::RowVectorXd::Random(2);
+  inputs << 1.0, 2.0, 3.0, 2.5, 
+            2.0, 5.0, -1.0, 2.0,
+            -1.5, 2.7, 3.3, -0.8;
+  weights << 0.2, 0.8, -0.5, 1.0,
+             0.5, -0.91, 0.26, -0.5,
+             -0.26, -0.27, 0.17, 0.87;
+  biases << 2.0, 3.0, 0.5;
+  weights2 << 0.1, -0.14, 0.5,
+              -0.5, 0.12, -0.33,
+              -0.44, 0.73, -0.13;
+  biases2 << -1.0, 2.0, -0.5;
 
-    std::cout << "Input: \n" << input << std::endl;
+  layer1_outputs = (inputs * weights.transpose());
+  layer1_outputs.rowwise() += biases;
 
-    layer1_out = relu((input * layer1).rowwise() + layer1_b);
-    std::cout << "Layer 1 output: \n" << layer1_out << std::endl;
-    
-    layer2_out = relu((layer1_out * layer2).rowwise() + layer2_b);
-    std::cout << "Layer 2 output: \n" << layer2_out << std::endl;   
+  layer2_outputs = (layer1_outputs * weights2.transpose());
+  layer2_outputs.rowwise() += biases2;
 
-    layer3_out = relu((layer2_out * layer3).rowwise() + layer3_b);
-    std::cout << "Layer 3 output: \n" << layer3_out << std::endl;
+  std::cout << std::endl;
+  std::cout << ">>>\n" << layer1_outputs << std::endl;
+  std::cout << ">>>\n" << layer2_outputs << std::endl;
+  std::cout << std::endl;
 
-    return 0;
+
+  return 0;
 }
+
